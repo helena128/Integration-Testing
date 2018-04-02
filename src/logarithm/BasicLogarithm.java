9daa1210430b27349d4d67fbe8a30701cc4dcf81
Elena Cheprasova <hellogbel@gmail.com>
2018-04-02 23:56:51 +0300

    First attempt to implement basic algorithm

---
+++ src/logarithm/BasicLogarithm.java
@@ -0,0 +1,48 @@
+package logarithm;
+
+public class BasicLogarithm {
+    private static final int MAX_ITERATIONS = 5000;
+    private Double eps;
+
+    public BasicLogarithm() {
+        this.eps = 0.000001;
+    }
+
+    public BasicLogarithm(Double eps) {
+        this.eps = eps;
+    }
+
+    public Double ln(Double x) {
+        double sum = 0.0, tmp = 0.0;
+
+        if (x < 0) {
+            throw new IllegalArgumentException("X must be >= 0!");
+        }
+
+        if (x == 0) {
+            return Double.NEGATIVE_INFINITY;
+        }
+
+        if (x == Double.POSITIVE_INFINITY) {
+            return x;
+        }
+
+        if (x <= 2) {
+            x -= 1;
+
+            for ( int i = 1; tmp < eps && i < MAX_ITERATIONS; i += 2 ) {
+                tmp = (Math.pow(x, i)/(double) i - Math.pow(x, i + 1)/(double)(i + 1));
+                sum += tmp;
+            }
+        } else {
+            x = (x / (x - 1));
+
+            for ( int i = 1; tmp < eps && i < MAX_ITERATIONS; i ++ ) {
+                tmp = 1.0 / (i * Math.pow(x, i));
+                sum += tmp;
+            }
+        }
+        return sum;
+
+    }
+}
---
+++ test/functions/LnTester.java
@@ -0,0 +1,28 @@
+package functions;
+
+import logarithm.BasicLogarithm;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import static java.lang.Math.log;
+
+public class LnTester {
+    private BasicLogarithm basicLogarithm;
+
+    @Before
+    public void setUp() {
+        basicLogarithm = new BasicLogarithm();
+    }
+
+    @Test
+    public void testCases() {
+        assertEquals(log(2), basicLogarithm.ln(2.0), 0.2);
+        assertEquals(log(Double.POSITIVE_INFINITY), basicLogarithm.ln(Double.POSITIVE_INFINITY), 0.2);
+        try {
+            assertEquals(log(Double.NEGATIVE_INFINITY), basicLogarithm.ln(Double.NEGATIVE_INFINITY), 0.2);
+        } catch (IllegalArgumentException ex) { /* do nothing */ }
+        assertEquals(log(0.0), basicLogarithm.ln(0.0), 0.2);
+    }
+}
