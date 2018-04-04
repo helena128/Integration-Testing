af21734d03b997012f70f32ed29905cb6d0fa620
Elena Cheprasova <hellogbel@gmail.com>
2018-04-04 09:29:54 +0300

    Implementing tests for trig function

--- test/functions/TrigFunctionTest.java
+++ test/functions/TrigFunctionTest.java
@@ -1,4 +1,14 @@
 package functions;
 
+import main.trigeometry.ITrigFunction;
+import main.trigeometry.TrigeometricFunction;
+import org.junit.Before;
+
 public class TrigFunctionTest {
+    private ITrigFunction fn;
+
+    @Before
+    public void setup() {
+        fn = new TrigeometricFunction()
+    }
 }
